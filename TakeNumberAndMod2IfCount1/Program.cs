1a9f782b5b74fd38d78852d78247813c114a8e38
Erik Egnatosyan <egnatosyan.sans@mail.ru>
2023-03-08 23:51:36 +0400

    not work "TakeNumberAndMod2IfCount1"

---
+++ TakeNumberAndMod2IfCount1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TakeNumberAndMod2IfCount1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string ns = n.ToString();
+            string temp = "";
+            int sum = 0;
+            for (int i = 0; sum.ToString().Length > 1; i++)
+            {
+                if (sum.ToString().Length != 0)
+                    sum += Convert.ToInt32(ns[i].ToString());
+                if (i == ns.Length - 1)
+                    temp = sum.ToString();
+                if (temp.Length > 1)
+                    sum += Convert.ToInt32(temp[i].ToString());
+                if (temp.Length == 1)
+                    sum %= 2;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
