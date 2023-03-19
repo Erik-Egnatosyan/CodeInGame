cfbd923d4561ca61c67a2a0d23c6af92f0e594e5
Erik Egnatosyan <egnatosyan.sans@mail.ru>
2023-03-19 21:43:44 +0400

    Add prime Number checker

---
+++ PrimeNumberCouter/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PrimeNumberCouter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            bool[] primes = new bool[n + 1];
+            int count = 0;
+            for (int i = 2; i <= n; i++)
+            {
+                primes[i] = true;
+            }
+            for (int i = 2; i * i <= n; i++)
+            {
+                for (int j = i * i; j <= n; j += i)
+                {
+                    primes[j] = false;
+                }
+            }
+            for (int i = 2; i <= n; i++)
+            {
+                if (primes[i])
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("\nКоличество простых чисел: " + count);
+        }
+    }
+}
