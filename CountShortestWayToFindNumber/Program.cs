d36848d06aa0ebc89715d5174b47e5b2fff9c6b2
Erik Egnatosyan <egnatosyan.sans@mail.ru>
2023-04-14 19:05:23 +0400

    add CountShortestWayToFindNumber

---
+++ CountShortestWayToFindNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CountShortestWayToFindNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] coins = { 25, 10, 5, 1 }; // Доступные монеты
+            int target = 60; // Целевая сумма
+
+            List<int> result = new List<int>(); // Результат
+            int total = 0; // Общая сумма монет
+
+            foreach (int coin in coins)
+            {
+                while (total + coin <= target)
+                {
+                    result.Add(coin);
+                    total += coin;
+                }
+            }
+            Console.WriteLine("Минимальное количество монет: " + result.Count);
+            Console.WriteLine("Монеты: " + string.Join(", ", result));
+            Console.WriteLine(string.Join(", ", coins));
+        }
+    }
+}
---
+++ DaysInMonth/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DaysInMonth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine(DateTime.DaysInMonth(y,m));
+        }
+    }
+}
