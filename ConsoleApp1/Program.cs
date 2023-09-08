551c14e3bc3a78358ee06df3016df156736d1e6a
Erik Egnatosyan <egnatosyan.sans@mail.ru>
2023-09-08 20:32:56 +0400

    Add Changes

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] ints = { 0,0,0,0,1,0,0,0,0,1,1,0,0,0,0,1,0,0,0,0 };
+        int count = 0;
+        int col = 0;
+        for (int i = 0; i < ints.Length; i++)
+        {
+            if (ints[i] == 1 && count == 0)
+                count++;
+            else if (count > 0 && ints[i] == 0)
+                col++;
+        }
+        Console.WriteLine(col);
+    }
+}
+
---
+++ ConsoleApp1/SomeClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1;
+internal class SomeClass : IDisposable
+{
+    public SomeClass()
+    {
+        Console.WriteLine("Mau");
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine("Im cleaning for you!");
+    }
+}
